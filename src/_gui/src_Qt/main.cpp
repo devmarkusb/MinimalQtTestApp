ce27de1de0bab1e97c695eb13dc4fd38da770d43
MarkusB <markusb81@web.de>
2017-05-17 23:35:19 +0200

    finally capable to be built for pure UWP (added asset icons+splash, manifest, cmake adaptions)

--- src/_gui/src_Qt/main.cpp
+++ src/_gui/src_Qt/main.cpp
@@ -1,11 +1,26 @@
+#include "Toolib/date_time/date_time.h"
+#include "Toolib/trace.h"
 #include "Toolib/PPDefs/MSVC/SUPPRESS_WARNINGS_EXTERNAL_BEGIN"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "Toolib/PPDefs/MSVC/SUPPRESS_WARNINGS_EXTERNAL_END"
 
+namespace test
+{
+void pullInRuntimeDependency()
+{
+    std::pair<too::date_time::Years, too::date_time::Months> ym{1, 14};
+    too::date_time::normalize::do_it(ym);
+    too::trace("INFO") << ym.first << ", " << ym.second;
+}
+}
 
 int main(int argc, char* argv[])
 {
+    too::tracer::init();
+
+    test::pullInRuntimeDependency();
+
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
