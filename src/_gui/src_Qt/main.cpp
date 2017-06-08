01eb4ae409714bf27e62e60f5ef8eb2b8e0feb94
MarkusB <markusb81@web.de>
2017-06-08 21:37:11 +0200

    background color set for UWP ico's + comments

--- src/_gui/src_Qt/main.cpp
+++ src/_gui/src_Qt/main.cpp
@@ -7,23 +7,25 @@
 
 namespace test
 {
+// for testing, whether winrt app package will contain the needed dll dependency (turns out it does)
 void pullInRuntimeDependency()
 {
     std::pair<too::date_time::Years, too::date_time::Months> ym{1, 14};
     too::date_time::normalize::do_it(ym);
     too::trace("INFO") << ym.first << ", " << ym.second;
 }
-}
+} // test
 
 int main(int argc, char* argv[])
 {
     too::tracer::init();
 
-    test::pullInRuntimeDependency();
-
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
     engine.load(QUrl("qrc:/MinimalQtTestApp/AppWindow.qml"));
+
+    test::pullInRuntimeDependency();
+
     return app.exec();
 }
