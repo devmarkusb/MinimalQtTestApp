ce7bee97b918579f64c3a9259d5221eeecd6a1f7
MarkusB <markusb81@web.de>
2018-08-15 22:11:37 +0200

    decided to keep some additional test code; sdk updates that hopefully fix the Android problems within C++ shared lib interfaces

--- src/_gui/src_Qt/main.cpp
+++ src/_gui/src_Qt/main.cpp
@@ -1,9 +1,18 @@
 #include "toolib/date_time/date_time.h"
+#include "toolib/ptr2ref.h"
 #include "toolib/trace.h"
+#include "uiwrap/filesys/filesys.h"
 #include "toolib/PPDefs/MSVC/SUPPRESS_WARNINGS_EXTERNAL_BEGIN"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "toolib/PPDefs/MSVC/SUPPRESS_WARNINGS_EXTERNAL_END"
+#include <fstream>
+
+
+namespace os
+{
+uiw::IFileSys& filesys() { return too::Ptr2Ref(uiw::IFileSys::getInstance()); }
+} // os
 
 namespace test
 {
@@ -19,6 +28,15 @@ void pullInRuntimeDependency()
 int main(int argc, char* argv[])
 {
     too::tracer::init();
+
+    // block for testing file writing and interface access (had some strange problems on Android)
+    std::ofstream f{"testfile.txt"};
+    f << "start" << std::endl;
+    std::string s;
+    s = os::filesys().getSystemPath(uiw::IFileSys::ESysPathType::APPDATA_writable, true);
+    f << "str" << std::endl;
+    f << s << std::endl;
+    f << "/str" << std::endl;
 
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
